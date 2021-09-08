18e94c3a30aff4c55a6d6fd176333e8be4dba157
ckjdelhi <chandan.krk@gmail.com>
2021-09-08 06:38:45 +0530

    Updates

---
+++ precache-manifest.e53e50554f7b3906aa9e224b4ca1ecf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7efd1e5f121801fa0dff8ee9904cbb",
+    "url": "/spacex-ui/index.html"
+  },
+  {
+    "revision": "97b33f6fa99bfc5c3c8c",
+    "url": "/spacex-ui/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "3a5d5dd1f740dc1555d5",
+    "url": "/spacex-ui/static/css/main.4791784f.chunk.css"
+  },
+  {
+    "revision": "97b33f6fa99bfc5c3c8c",
+    "url": "/spacex-ui/static/js/2.27180697.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/spacex-ui/static/js/2.27180697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5d5dd1f740dc1555d5",
+    "url": "/spacex-ui/static/js/main.d975c132.chunk.js"
+  },
+  {
+    "revision": "668054cb881b17d40351",
+    "url": "/spacex-ui/static/js/runtime-main.3a642846.js"
+  }
+]);
